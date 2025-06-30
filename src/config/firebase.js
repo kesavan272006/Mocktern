e839f66fad750b12f7da6bc4bed7db0f4edbed1f
Kesavan G <kesavannivesh272006@gmail.com>
2025-06-30 19:14:59 +0530

    added safety measures

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,18 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAODcAMNTG8I5ttp8mOIwxGvG6XBBnhP_k",
-  authDomain: "mocktern.firebaseapp.com",
-  projectId: "mocktern",
-  storageBucket: "mocktern.firebasestorage.app",
-  messagingSenderId: "895893740588",
-  appId: "1:895893740588:web:6813aeb0f54208d6e38589",
-  measurementId: "G-S7PTNSYM9F"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 export const auth = getAuth(app);
-export const googleprovider = new GoogleAuthProvider(app);
+export const googleprovider = new GoogleAuthProvider();
 export const database = getFirestore(app);
